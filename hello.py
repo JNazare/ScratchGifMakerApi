c645ef537338206409b15f51d5f32e826eb7a93a
jnazare <jules.nazare@gmail.com>
2015-05-20 19:26:17 -0400

    working

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, send_file
 from werkzeug import secure_filename
 import os
 from moviepy.editor import *
+from PIL import Image
 
 UPLOAD_FOLDER = '/Users/Juliana/Desktop'
+GIF_FILE_PATH = '/Users/Juliana/Desktop/test.gif'
 ALLOWED_EXTENSIONS = set(['MOV'])
 
 app = Flask(__name__)
@@ -12,11 +14,17 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+        filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 def make_gif(filename):
-    clip = (VideoFileClip(filename).rotate(-90).resize(0.3))
-    clip.write_gif("/Users/Juliana/Desktop/test.gif", fps=1)
+    clip = (VideoFileClip(filename).rotate(-90).resize(0.5))
+    clip.write_gif(GIF_FILE_PATH, fps=1)
+    with open(GIF_FILE_PATH, "rb") as f:
+        gif = f.read()
+        encoded_gif = gif.encode("base64")
+    return encoded_gif
+    # gif = Image.open(GIF_FILE_PATH)
+    # return gif#send_file(GIF_FILE_PATH, mimetype="image/gif", as_attachment=True)
 
 @app.route('/', methods=["GET", "POST"])
 def upload_file():
@@ -26,8 +34,8 @@ def upload_file():
             filename = secure_filename(file.filename)
             path_on_comp = os.path.join(app.config["UPLOAD_FOLDER"], filename)
             file.save(path_on_comp)
-            make_gif(path_on_comp)
-            return "DONE!"
+            gif = make_gif(path_on_comp)
+            return gif
     
     return 'Hello World!'
 
